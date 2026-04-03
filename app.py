a705fb93c43d002e684dd77b3e93a8838545fcdb
sohailouaby <sohailouaby23@gmail.com>
2026-04-03 21:45:21 +0100

    Added the rough dhape of the website
    i added a nav bar, some css, and 2 more html files with placeholder text in them

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/templates/index.html")
 def home():
     return render_template("index.html")
+
+@app.route("/templates/about.html")
+def about():
+    return render_template("about.html")
+
+@app.route("/templates/guns.html")
+def guns():
+    return render_template("guns.html")
 
 
 if __name__ == "__main__":
